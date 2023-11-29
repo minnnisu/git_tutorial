8546d0896d52780082e8053a9c4b0804a4aeabb9
minnnisu <mine52100@naver.com>
2023-11-29 21:03:34 +0900

    ADD: hello git

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// EJS를 뷰 엔진으로 설정
+app.set("view engine", "ejs");
+
+// 정적 파일 서비스 설정 (public 폴더에는 스타일 시트 및 기타 정적 파일이 있을 수 있습니다)
+app.use(express.static("public"));
+
+// 루트 경로에 대한 라우터 설정
+app.get("/", (req, res) => {
+  //   res.render("index", { title: "Express with EJS" });
+  res.send("Hello Git");
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
